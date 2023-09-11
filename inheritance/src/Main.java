e81f6fadc8f55ae69aec2b08ae506cdbd0212bfd
Cem Vural Yılmaz <127853144+1220505067@users.noreply.github.com>
2023-09-12 01:02:38 +0300

    inheritance

---
+++ inheritance/src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Employee employee= new Employee();
+
+        EmployeeManager employeeManager=new EmployeeManager();
+        CustomerManager customerManager=new CustomerManager();
+
+
+
+
+
+    }
+}
