29c2fdb02dc2a0d33981c1feead633e5a2499b78
Cem Vural Yılmaz <127853144+1220505067@users.noreply.github.com>
2023-09-13 01:23:50 +0300

    polymorphism

---
+++ polymorphismDemo/src/Main.java
@@ -0,0 +1,13 @@
+//polymorphism = çok biçimlilik
+public class Main {
+    public static void main(String[] args) {
+//       BaseLogger[] loggers = new BaseLogger[]{new FileLogger(),new DatabaseLogger(),new EmailLogger(),new ConsoleLogger()};
+//       for (BaseLogger logger:loggers){
+//           logger.Log("Log mesajı");
+//       }
+
+            CustomerManager customerManager = new CustomerManager(new DatabaseLogger());
+            customerManager.add();
+
+    }
+}
